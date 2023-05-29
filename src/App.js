d4133d635c33ea7e4eaa20f3f1ca96981776b87d
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-05-29 14:42:33 +0300

    Update App.js for checking CI/CD

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(process.env);
+  console.log('env', { env: process.env });
 
   return (
     <div className="App">
